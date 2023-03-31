33ddf5fbbcbaa4774fa134580b3fff09f0e0d877
gowthamshan02 <108513970+gowthamshan02@users.noreply.github.com>
2023-03-31 11:59:51 +0530

    Add files via upload

---
+++ src/components/Guns.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Card from './Card'
+import "../pages/Guns.css"
+export default function Armors() {
+  return (
+    <>
+    <section id='header2'>
+      <h1>Type Of Guns & Exports</h1>
+      <Card/>
+    </section>
+    </>
+  )
+}
